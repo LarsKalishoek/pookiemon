817fb401001d06e2db3c1d9c2042f931e40003c0
LarsKalishoek1 <lars.kalishoek@student.divd.nl>
2023-05-12 16:54:04 +0200

    charmander class gemaakt

---
+++ pokemonBattleSIm/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace pokemonBattleSIm;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Charmander charmander = new Charmander("charmander", "fire", "water", 100);
+                
+        bool x = true;
+        while (x)
+        {
+            Console.WriteLine("Enter Charmander's name: ");
+            string char_name = Console.ReadLine();
+            charmander.setName(char_name);
+            Console.WriteLine("Name: " + charmander.getName());
+            Console.WriteLine("Type: " + charmander.getType());
+            Console.WriteLine("Weakness: " + charmander.getWeakness());
+            Console.WriteLine("Hp: " + charmander.getHp());
+            for (int i = 0; i <= 10; i++)
+            {
+                charmander.BattleCry();
+            }
+            Console.WriteLine("Do you want to continue y/n: ");
+            string check_state = Console.ReadLine().ToLower();
+            if (check_state == "y")
+            {
+                x = true;
+            }
+            else
+            {
+                x = false; break;
+            }
+        }
+    }
+        
+}
+    
---
+++ pokemonBattleSIm/charmander.cs
@@ -0,0 +1,41 @@
+using System;
+
+
+
+class Charmander
+{
+  public string name;
+  public string type;
+  public string weakness;
+  public int hp;
+
+  public Charmander(string name, string type, string weakness, int hp)
+  {
+    this.name = name;
+    this.type = type;
+    this.weakness = weakness;
+    this.hp = hp;
+  }
+  public string getName()
+  {
+      return name;
+  }
+  public string getType()
+  {
+      return type;
+  }
+  public string getWeakness()
+    { return weakness;}
+
+  public int getHp()
+    { return hp; }
+
+  public void setName(string name)
+  {
+      this.name = name;
+  }
+  public void BattleCry()
+    {
+        Console.WriteLine(this.name); 
+    }
+}
